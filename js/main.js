84f11f27cc5db99d41207d1da6142052fcc2dca5
UPennPhiDE <ericsc@sas.upenn.edu>
2017-03-28 00:42:48 -0400

    Sima

--- js/main.js
+++ js/main.js
@@ -1,53 +1,129 @@
-$(document).ready(function(){
+ jQuery(document).ready(function($){
+$(".all-portfolios").isotope({
+itemSelector : '.single-portfolio',
 
-
-	$("#portfolio-contant-active").mixItUp();
-
-
-	$("#testimonial-slider").owlCarousel({
-	    paginationSpeed : 500,      
-	    singleItem:true,
-	    autoPlay: 3000,
-	});
-
-
-
-
-	$("#clients-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
-
-	$("#works-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
-
-
-	// google map
-		var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
-		  });
-		}
-
-
-	// Counter
-
-	$('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
-
+layoutMode: 'fitRows',
+});
+$('ul.filter li').click(function(){
+$("ul.filter li").removeClass("active");
+$(this).addClass("active");
+ 
+var selector = $(this).attr('data-filter');
+$(".all-portfolios").isotope({
+	
+filter: selector,
+animationOptions: {
+duration: 750,
+easing: 'linear',
+queue: false,
+}
+});
+return false;
+});
 });
 
 
 
+
+jQuery(document).ready(function () {
+
+	
+/*----------------------------------------------------*/
+/*  Animated Progress Bars
+/*----------------------------------------------------*/
+
+    jQuery('.skills li').each(function () {
+        jQuery(this).appear(function() {
+          jQuery(this).animate({opacity:1,left:"0px"},800);
+          var b = jQuery(this).find(".progress-bar").attr("data-width");
+          jQuery(this).find(".progress-bar").animate({
+            width: b + "%"
+          }, 1300, "linear");
+        }); 
+    });   
+
+/*----------------------------------------------------*/
+/* Crousel Team 
+/*----------------------------------------------------*/
+	$('.all_team').owlCarousel({
+		items:4,
+		loop:true,
+		margin:10,
+		nav:true,
+		autoplay:true,
+		smartSpeed:3000,
+		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:4
+			}
+		}
+	})
+	
+/*----------------------------------------------------*/
+/* Testimonial crousel 
+/*----------------------------------------------------*/
+	$('.all_tstm').owlCarousel({
+		items:1,
+		loop:true,
+		margin:10,
+		nav:true,
+		autoplay:true,
+		smartSpeed:3000,	
+		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	})
+	
+/*----------------------------------------------------*/
+/* Happy Client crousel 
+/*----------------------------------------------------*/
+	$('.al_clt').owlCarousel({
+		items:6,
+		loop:true,
+		margin:30,
+		nav:true,
+		autoplay:true,
+		smartSpeed:3000,	
+		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:6
+			},
+			1000:{
+				items:6
+			}
+		}
+	})
+
+	
+	
+/*preloder*/
+$(window).load(function() { // makes sure the whole site is loaded
+	$('#status').fadeOut(); // will first fade out the loading animation
+	$('#loader-wrapper').delay(200).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(200).css({'overflow-x':'hidden'});
+})	
+	
+	
+	
+});
 
